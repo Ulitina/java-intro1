d871912ee15b15d5e198c50aef39d64b2cecbfd5
iuliia ulitina <jul.ulitina@gmail.com>
2019-02-26 14:28:21 +0200

    lesson3

--- src/lesson02/StringTest.java
+++ src/lesson02/StringTest.java
@@ -3,7 +3,7 @@ package lesson02;
 public class StringTest {
      public void everyNth(String a, int n){
          String b = a.trim();
-         for(int i=n-1; i <= b.length(); i = i+n-1){
+         for(int i=n-1; i < b.length(); i = i+n){
              System.out.print(b.charAt(i));
          }
      }
---
+++ src/lesson03/PassengerCar.java
@@ -0,0 +1,31 @@
+package lesson03;
+
+import lesson02.Person;
+
+public class PassengerCar extends Car implements Climatable{
+
+    private Person[] persones;
+
+    public PassengerCar(String manufacturer, int year, String color) {
+        super(manufacturer, year, color);
+    }
+
+    public PassengerCar(String manufacturer, int year, String color, Engine engine) {
+        super(manufacturer, year, color, engine);
+    }
+
+    @Override
+    public void climateControle(int tempr) {
+        System.out.println("Air condition is trying to make temperature " + tempr);
+    }
+
+    @Override
+    public void climateStop() {
+        System.out.println("Passenger car's climate control system is turned off");
+    }
+
+    @Override
+    protected String getName() {
+        return "Passenger car";
+    }
+}
