0fc1d69de558797a3fd1e0fad8838dce7d54db2c
jul.ulitina <jul.ulitina@gmail.com>
2019-02-26 09:01:50 +0200

    Hometask_2

--- src/lesson02/Program2.java
+++ src/lesson02/Program2.java
@@ -1,10 +1,28 @@
 package lesson02;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
-        Person p1 = new Person("Vasya", "Pupkin", 32);
+        /*Person p1 = new Person("Vasya", "Pupkin", 32);
         Person p2 = new Person("Tanya", "Ivanova", 30);
 
-        System.out.println(p1);
+        System.out.println(p1);*/
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input text: ");
+
+        String text = in.nextLine();
+
+        if (text.isEmpty()) {
+            System.out.println("Enter some text");
+        }
+        else {System.out.print("Enter a number that is less or equal to text length: ");
+            int i = in.nextInt();
+            if (i < 0 ||i > text.length()){
+                System.out.print("Enter a number that is less or equal to text length: ");
+            }
+            else {
+                StringTest st1 = new StringTest();
+                st1.everyNth(text, i);
     }
-}
+}}}
---
+++ src/lesson02/StringTest.java
@@ -0,0 +1,10 @@
+package lesson02;
+
+public class StringTest {
+     public void everyNth(String a, int n){
+         String b = a.trim();
+         for(int i=n-1; i <= b.length(); i = i+n-1){
+             System.out.print(b.charAt(i));
+         }
+     }
+}
